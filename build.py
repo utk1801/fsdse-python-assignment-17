442c7b399f5f1a1c211f24ae1efa304dcf21324e
utk1801 <utk1801@users.noreply.github.com>
2017-06-17 09:36:28 +0000

    Done

--- build.py
+++ build.py
@@ -2,13 +2,15 @@ import operator
 
 
 def solution_asc(dic):
-    '''
-    Enter your code here
-    '''
+    ascDic = sorted(dic.items(),key = operator.itemgetter(0))
+    print ascDic
+    return ascDic
 
 
 def solution_desc(dic):
-    '''
-    Enter your code here
-    '''
+    dscDic = sorted(dic.items(),key = operator.itemgetter(0),reverse = True)
+    print dscDic
+    return dscDic
 
+solution_asc({1: 2, 3: 4, 4: 3, 2: 1, 0: 0})
+solution_desc({1: 2, 3: 4, 4: 3, 2: 1, 0: 0})
